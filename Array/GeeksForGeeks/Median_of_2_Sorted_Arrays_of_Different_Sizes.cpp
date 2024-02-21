8a8962b46b7f342a902d51bb221179304d2d2511
tech-saurabh382 <saurabhgawali3008@gmail.com>
2024-02-22 01:49:44 +0530

    Median of 2 Sorted Arrays of Different Sizes

---
+++ Array/GeeksForGeeks/Median_of_2_Sorted_Arrays_of_Different_Sizes.cpp
@@ -0,0 +1,41 @@
+// GFG Problem no.
+// Median of 2 Sorted Arrays of Different Sizes -> https://www.geeksforgeeks.org/problems/median-of-2-sorted-arrays-of-different-sizes/1?itm_source=geeksforgeeks&itm_medium=article&itm_campaign=bottom_sticky_on_article
+
+// Question Description
+
+// Given two sorted arrays array1 and array2 of size m and n respectively. Find the median of the two sorted arrays.
+
+// Example 1:
+
+// Input:
+// m = 3, n = 4
+// array1[] = {1,5,9}
+// array2[] = {2,3,6,7}
+// Output: 5
+// Explanation: The middle element for
+// {1,2,3,5,6,7,9} is 5
+// Example 2:
+
+// Input:
+// m = 2, n = 4
+// array1[] = {4,6}
+// array2[] = {1,2,3,5}
+// Output: 3.5
+// Your Task:
+// The task is to complete the function MedianOfArrays() that takes array1 and array2 as input and returns their median.
+
+// Can you solve the problem in expected time complexity?
+
+// Expected Time Complexity: O(min(log n, log m)).
+// Expected Auxiliary Space: O((n+m)/2).
+
+// Constraints:
+// 0 ≤ m,n ≤ 106
+// 1 ≤ array1[i], array2[i] ≤ 109
+
+// Company Tags
+
+// Amazon Microsoft Samsung Google
+
+// ********* Solution **********
+
