4356add36afb25be7cc1277c2eabbc0f0c2db233
tech-saurabh382 <saurabhgawali3008@gmail.com>
2024-03-02 17:30:04 +0530

    Palindrome String

---
+++ Strings/GeeksForGeeks/Palindrome_String.cpp
@@ -0,0 +1,86 @@
+// GFG Problem
+// Palindrome String -> https://www.geeksforgeeks.org/problems/palindrome-string0817/1
+
+// Question Description
+
+// Given a string S, check if it is palindrome or not.
+
+// Example 1:
+
+// Input: S = "abba"
+// Output: 1
+// Explanation: S is a palindrome
+// Example 2:
+
+// Input: S = "abc"
+// Output: 0
+// Explanation: S is not a palindrome
+// Your Task:
+// You don't need to read input or print anything. Complete the function isPalindrome()which accepts string S and returns an integer value 1 or 0.
+
+// Expected Time Complexity: O(Length of S)
+// Expected Auxiliary Space: O(1)
+
+// Constraints:
+// 1 <= Length of S<= 2*105
+
+// Company Tags
+
+// Paytm Zoho Morgan Stanley Amazon D-E-Shaw FactSet Cisco Facebook
+
+// ********** Solution **********
+
+//{ Driver Code Starts
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function template for C++
+class Solution
+{
+public:
+    int isPalindrome(string S)
+    {
+        // Two Pointer Approach
+
+        // T.C -> O(n)
+        // S.C -> O(1)
+
+        int start = 0;
+        int end = S.size() - 1;
+        while (start <= end)
+        {
+            if (S[start] != S[end])
+            {
+                return 0;
+            }
+            start++, end--;
+        }
+        return 1;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        string s;
+        cin >> s;
+
+        Solution ob;
+
+        cout << ob.isPalindrome(s) << "\n";
+    }
+
+    return 0;
+}
+// } Driver Code Ends
